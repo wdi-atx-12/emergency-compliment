f1e8ef3ca93c55be17902797397841b2bf726f95
Peter Kong <bansuki@gmail.com>
2017-10-02 19:26:47 -0500

    initial file setup

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+let compliments = [
+"Your instructors love you",
+"High five = ^5",
+"Chris thinks you\'re wicked smart!",
+"Britney Jo is sooo proud of you! :)",
+"Nicole would totally hire you.",
+"It\'s almost beer o\'clock!",
+"You\'re a full-stack unicorn! 🦄"
+]
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render('index', {comp: getCompliment()});
+})
+
+app.listen(3000, function () {
+  console.log('listening!');
+})
+
+function getCompliment() {
+  return compliments[Math.floor(Math.random() * compliments.length)];
+}
