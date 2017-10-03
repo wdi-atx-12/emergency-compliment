f601711bd46069a34b670967375af9c33e8c25f8
Peter Kong <bansuki@gmail.com>
2017-10-03 10:27:15 -0500

    removed extraneous code

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public')); // points to css
 
 let compliments = [
 "Your instructors love you",
