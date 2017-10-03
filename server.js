2b0927a0e957b3ca2c132bf7415c83cea61bd9a0
Karla Moreno <moreno_karla@live.com>
2017-10-03 01:53:38 -0500

    random compliments show on each refresh, need to add name query function

--- server.js
+++ server.js
@@ -2,12 +2,28 @@
 const express = require('express');
 const app = express();
 
+var compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Chris thinks you\'re wicked smart!",
+  "Britney Jo is sooo proud of you! :)",
+  "Nicole would totally hire you.",
+  "It\'s almost beer o\'clock!",
+  "You\'re a full-stack unicorn! 🦄"
+]
+
 // app code
 app.get('/', function(req, res){
-  rezz.send('SQUIRRELY SQUIRREL SQUANCHES SQUIRRELS');
+  var complimentIndex = Math.floor(Math.random()*compliments.length);
+  var compliment = compliments[complimentIndex];
+  res.send('<h1>Hey, squirrel.</h1>' + compliment + ' <a href=#>Another?</a>');
+})
+
+app.get('/:name', function(req, res){
+  console.log('name entered');
 })
 
 // app start
 app.listen(3000, function(){
-  console.log('http server in bay 30000 ready for liftoff');
+  console.log('http server ready for client vessel in port 3000');
 });
