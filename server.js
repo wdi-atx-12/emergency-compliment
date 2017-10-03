7823cefe154b9fc83ffd2e1f33390bb7815ef9ff
Jesse Lewis <jesseaustinlewis@gmail.com>
2017-10-02 21:18:28 -0500

    added color and name change functionality

--- server.js
+++ server.js
@@ -2,16 +2,41 @@
 const express = require('express');
 const app = express();
 
+// View engine setup
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use('/', express.static('public'));
+
+
 // Call routes
-app.get('/', function(res, req){
-  res.send("Hello from serverjs we are on port 3000.");
-  console.log('i started');
+app.get('/', function(req, res){
+  var compliments = [
+    "Your instructors love you",
+    "High five = ^5",
+    "Chris thinks you\'re wicked smart!",
+    "Britney Jo is sooo proud of you! :)",
+    "Nicole would totally hire you.",
+    "It\'s almost beer o\'clock!",
+    "You\'re a full-stack unicorn! 🦄"
+    ];
+  var randcompliment = compliments[Math.floor(Math.random()*compliments.length)];
+  res.render('index', { passcompliment: randcompliment});
 });
 
-// // get personal.
-// app.get('/:name', function(res, req){
-//   res.redirect('personal');
-// });
+app.get('/:name', function(req, res){
+  var name = req.params.name;
+  var compliments = [
+    "Your instructors love you",
+    "High five = ^5",
+    "Chris thinks you\'re wicked smart!",
+    "Britney Jo is sooo proud of you! :)",
+    "Nicole would totally hire you.",
+    "It\'s almost beer o\'clock!",
+    "You\'re a full-stack unicorn! 🦄"
+    ];
+  var randcompliment = compliments[Math.floor(Math.random()*compliments.length)];
+  res.render('personal', { name: name, passcompliment: randcompliment });
+});
 
 // Start server
 app.listen(3000, function(){
